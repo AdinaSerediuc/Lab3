e89ede55b8d67da28b86ef692fac7b5ddb8c1885
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-10-02 23:59:25 +0300

    Lab3Prob3

---
+++ Lab3Prob3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3Prob3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Ex 3
+            Scrieti un program care sa verifice daca un sir de caractere citit de la tastatura este sau nu palindrom
+            */
+
+            Console.Write("Introduceti un sir de caractere: ");
+            string input = Console.ReadLine();
+
+            string cleanedInput = input.Replace(" ", "").ToLower();
+
+            if (IsPalindrome(cleanedInput))
+            {
+                Console.WriteLine("Este un palindrom.");
+            }
+            else
+            {
+                Console.WriteLine("Nu este un palindrom.");
+            }
+        }
+
+        static bool IsPalindrome(string text)
+        {
+            int left = 0;
+            int right = text.Length - 1;
+
+            while (left < right)
+            {
+                if (text[left] != text[right])
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+}
