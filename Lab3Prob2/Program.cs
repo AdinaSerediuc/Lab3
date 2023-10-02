e9884dc15ea29bef1c0e147e7e3e169994a95249
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-10-02 21:37:18 +0300

    Lab3Prob2

---
+++ Lab3Prob2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3Prob2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
