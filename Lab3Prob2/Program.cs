fea24deb8fe751089fa9b8eafc8f93fe973623c7
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-10-02 22:57:50 +0300

    Prob2

--- Lab3Prob2/Program.cs
+++ Lab3Prob2/Program.cs
@@ -12,6 +12,30 @@ namespace Lab3Prob2
              * Ex 2
              Scrieti un program care va afisa pozitia unui substring intr un string, ambele siruri de caractere fiind citite de la tastatura
             */
+
+                Console.WriteLine("Introduceti un sir de caractere:");
+                string text = Console.ReadLine();
+
+                Console.WriteLine("Introduceti subsirul cautat:");
+                string subString = Console.ReadLine();
+
+                int pozitie = GasestePozitieSubString(text, subString);
+
+            if (pozitie != -1)
+            {
+                Console.WriteLine("Subsirul " + subString + " se afla la pozitia " + pozitie + " in sirul dat.");
+            }
+
+            else
+            {
+                Console.WriteLine("Subsirul " + subString + " nu a fost gasit in sirul dat.");
+            }
+            }
+
+            static int GasestePozitieSubString(string text, string subString)
+            {
+                int pozitie = text.IndexOf(subString);
+                return pozitie;
+            }
         }
     }
-}
